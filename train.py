b8aa6f6e82064842cb64ddccbc6bf0202ce2df83
hong6us <41441659+hong6us@users.noreply.github.com>
2020-06-17 11:38:13 -0600

    Update train.py

--- train.py
+++ train.py
@@ -309,7 +309,8 @@ def train(hyp):
                                              model=ema.ema,
                                              single_cls=opt.single_cls,
                                              dataloader=testloader,
-                                             fast=ni < n_burn)
+                                             fast=ni < n_burn,
+                                             verbose=True)
 
         # Write
         with open(results_file, 'a') as f:
